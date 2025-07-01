006ea046427494cdb9d9787da92611b7f5c473ad
Pranay DASARI <dpranaysai2@gmail.com>
2025-07-01 13:34:50 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/portfolio-website/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/dynamic-resume-showcase-pro/' : '/',
   server: {
     host: "::",
     port: 8080,
