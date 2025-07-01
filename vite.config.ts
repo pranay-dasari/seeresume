11e522c77a31137302faffbacb1d3e7ec05ab5bf
Pranay-Dasari <106328556+ps-dasari@users.noreply.github.com>
2025-07-01 14:12:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/dynamic-resume-showcase-pro/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/seeresume/' : '/',
   server: {
     host: "::",
     port: 8080,
